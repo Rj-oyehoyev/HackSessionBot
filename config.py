d903a7bcde6eccb8550bb5192bb478eb8c45720c
RJ-5 <124252004+RJ-5@users.noreply.github.com>
2023-02-07 23:08:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,4 @@ class Config:
     API_ID = "13146123"
     API_HASH = "53e10a257baaddea9c413a0c4249e731"
     TOKEN = "5898631697:AAEYcG3iKmi9rAqJQ33S4eO5o8xiifDycd0"  
-    START_PIC = "https://telegra.ph//file/971c9cf892b195cd7c679.jpg" 
-    CHAT = "-1001731284008"    
+    START_PIC = "https://telegra.ph//file/971c9cf892b195cd7c679.jpg"
